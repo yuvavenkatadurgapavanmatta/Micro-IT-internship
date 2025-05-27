a06550454d43be3d171916be542e42c8044bb632
yuvavenkatadurgapavanmatta <durgapavan69@gmail.com>
2025-05-27 16:21:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const useUpper = document.getElementById("uppercase").checked;
+  const useLower = document.getElementById("lowercase").checked;
+  const useNumbers = document.getElementById("numbers").checked;
+  const useSymbols = document.getElementById("symbols").checked;
+
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+  let chars = "";
+  if (useUpper) chars += upper;
+  if (useLower) chars += lower;
+  if (useNumbers) chars += numbers;
+  if (useSymbols) chars += symbols;
+
+  if (chars === "") {
+    document.getElementById("password").innerText = "Please select at least one option.";
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  document.getElementById("password").innerText = password;
+}
